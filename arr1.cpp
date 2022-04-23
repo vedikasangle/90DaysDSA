4679b605444a591ceefba8e4d4d1e55e9190e0a8
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2022-04-23 16:42:09 +0530

    Add files via upload

---
+++ arr1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+//linear search
+int main(){
+    int search(int arr[], int n, int x){
+    for(int i=0; i< n; i++){
+        if (arr[i] == x)
+            return i;
+    return -1;
+    }
+    }
+}
+
+
+
+    
