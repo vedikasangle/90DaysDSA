2edb3906de1eaa6072d1e7864752142965070f43
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2022-04-23 18:58:32 +0530

    Add files via upload

---
+++ arr3.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int deleteEle(int arr[], int n, int x)
+{
+    int i = 0;
+
+    for (i = 0; i < n; i++)
+    {
+        if (arr[i] == x)
+            break;
+    }
+    if (i == n)//; you had put ; by mistake and thats why desired output was not coming !!!!
+    return n;
+
+    for (int j = i; j < n - 1; j++)
+    {
+        arr[j] = arr[j + 1];
+    }
+    return n - 1;
+}
+
+int main()
+{
+    int arr[] = {3, 8, 12, 5, 6}, x = 12;
+    int n = 5;
+    cout << "Before deletion " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    n = deleteEle(arr, n, x);
+    cout << "After deletion " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
