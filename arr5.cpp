691a2ce4ae63bc268004b5389f17958786b1e1a6
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2022-04-28 19:33:50 +0530

    Add files via upload

---
+++ arr5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int getlargest(int arr[], int n)
+{
+    int res = 0;
+    for (int i = 1; i < n; i++)
+        if (arr[i] > arr[res])
+            res = i;
+    return res;
+}
+
+int main()
+{
+    int arr[] = {15, 12, 30, 90};
+    cout << getlargest(arr, 4);
+    return 0;
+}
