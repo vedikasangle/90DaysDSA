f83083223276728ce17e240776e0a4ad3ca56a0b
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2022-04-29 07:31:56 +0530

    Add files via upload

---
+++ seconlargestelementinanarray2.cpp
@@ -0,0 +1,42 @@
+ #include<iostream>
+ using namespace std ;
+ //efficient approach 
+
+
+
+int secondlargest(int a[],int n)
+{
+    int largest=0;
+    int res=-1;
+    
+    for(int i=1;i<n;i++)
+    {
+        if(a[i]>a[largest])
+        {
+            res=largest;
+            largest=i;
+        }
+        else if(a[i]!=a[largest])
+        {
+            if(res==-1||a[i]>a[res])
+            res=i;
+        }
+        
+    }
+    return res;
+}
+int main() 
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int pos=secondlargest(a,n);
+    printf("%d ",a[pos]);
+    
+	return 0;
+    
+}
